fc05547fec0f31c50b154eb558ef6484b3eb331f
zwnes <89182279+zszwnes@users.noreply.github.com>
2022-06-30 20:39:07 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,20 @@
 yum -y install wget
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-yum makecache
+# wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+# yum makecache
 
 yum install sudo -y 
 sudo yum -y update
-ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && sudo yum install -y git && npm config set registry https://registry.npmmirror.com
+ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && sudo yum install -y git
+npm config set registry https://registry.npmmirror.com
 echo 'export N_NODE_MIRROR="https://mirrors.ustc.edu.cn/node/"' | tee -a ~/.bashrc
 echo 'export N_NODE_DOWNLOAD_MIRROR="https://mirrors.ustc.edu.cn/node/"' | tee -a ~/.bashrc
 source ~/.bashrc
 
-curl -fsSL https://raw.githubusercontents.com/tj/n/master/bin/n | sudo -E bash -s lts
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | sudo -E bash -s lts
 sudo --preserve-env=PATH env npm install -g n
 sudo --preserve-env=PATH env n lts
-git clone https://gitcode.net/iSpeller/Adachi-BOT.git
+# git clone https://gitcode.net/iSpeller/Adachi-BOT.git
+git clone https://github.com/Arondight/Adachi-BOT.git
 cd ./Adachi-BOT/
 
 echo "安装chromium"
